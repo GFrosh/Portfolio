4b9a0c776c579d4bf6cadb9a96945c9abc6bc37c
Gideon Onyegbula <110903404+GFrosh@users.noreply.github.com>
2022-08-26 00:57:54 +0100

    initial comitm

---
+++ index.js
@@ -0,0 +1,9 @@
+//selecting html elements
+let inputElement = document.querySelector("#input")
+console.log(emailElement)
+console.log("hello world")
+
+
+//attaching click listener
+//getting user entered values
+//javascript  validations
